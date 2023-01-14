0601d4adec04c39a030cebe1b4290173d4912876
Preet1810 <preetpalsingh1239@gmail.com>
2023-01-14 16:51:23 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Helmet } from "react-helmet";
 
 import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
-const App = () => (
+const App=() => (
   <div>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>GERICHT</title>
+      <link rel="canonical" href="https://gpt3blog.netlify.app/" />
+      <meta name="description" content="GERICHT-RESTAURANT" />
+    </Helmet>
     <Navbar />
     <Header />
     <AboutUs />
